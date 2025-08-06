976a3697b9523838728fc15a8fda3f5d20fab88b
Valmir-Neto <netovalmirr@gmail.com>
2025-08-06 12:36:29 -0300

    Feat: login and registration page create completed

--- src/Pages/Registration.tsx
+++ src/Pages/Registration.tsx
@@ -1,7 +1,35 @@
+import { Box, Grid, Container } from '@mui/material'
+import { LoginImage } from '@/components'
+
 function Registration() {
   return (
     <>
-      <h1>registration</h1>
+      <Box sx={{ minHeight: '100dvh', width: '100%' }}>
+        <Grid container sx={{ width: '100%', minHeight: '100dvh' }}>
+          <Grid
+            size={{ xs: 12, md: 6 }}
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100dvh',
+            }}
+          >
+            <Container maxWidth={'sm'}>
+              <h1>Cadastro</h1>
+            </Container>
+          </Grid>
+          <Grid
+            size={6}
+            sx={{
+              minHeight: '100dvh',
+              display: { xs: 'none', md: 'block', lg: 'block', xl: 'block' },
+            }}
+          >
+            <LoginImage />
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
