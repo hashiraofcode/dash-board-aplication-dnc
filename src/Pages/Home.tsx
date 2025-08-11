54ae44af5f1cf69aa8f11c68cf0dd652b729ffaa
Valmir-Neto <netovalmirr@gmail.com>
2025-08-11 15:42:55 -0300

    Feat: create a header component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,8 @@
+import { Header } from '@/components/index.ts'
 function Home() {
   return (
     <>
+      <Header />
       <h1>Home</h1>
     </>
   )
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as FormApp } from '@/components/formAppComponent'
 export { default as LoginImage } from '@/components/loginImage'
 export { default as Logo } from '@/components/logo.tsx'
+export { default as Header } from '@/components/header.tsx'
